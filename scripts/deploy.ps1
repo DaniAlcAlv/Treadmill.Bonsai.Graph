83a6c7702e9447273c23364704c6a7a95cae634e
Dani Alcala Alvarez <daniel.alvarez@alleninstitute.org>
2025-10-16 22:19:04 -0700

    Removed venv

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -7,13 +7,6 @@ if (-not (Get-Command uv -ErrorAction SilentlyContinue)) {
     throw "The 'uv' command was not found. See https://docs.astral.sh/uv/getting-started/installation/ for instructions."
 }
 
-if (Test-Path -Path ./.venv) {
-    Remove-Item ./.venv -Recurse -Force
-}
-&python -m venv .venv
-.\.venv\Scripts\Activate.ps1
-# Write-Output "Synchronizing environment..."
-# &uv sync --extra launcher
 Write-Output "Creating a Bonsai environment and installing packages..."
 if (Test-Path -Path "bonsai") {
     Set-Location "bonsai"
